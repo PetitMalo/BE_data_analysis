439c4cb22e2c1eb804b2eee189d5abb677efaf53
dacher <mael.dacher@student.isae-supaero.fr>
2026-02-09 10:49:24 +0100

    debug

--- src/hyper_param_search.py
+++ src/hyper_param_search.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import numpy as np
 import optuna
 import xgboost as xgb
-from cuml.neural_network import MLPClassifier
+from sklearn.neural_network import MLPClassifier
 
 from src.utils import logger
 
