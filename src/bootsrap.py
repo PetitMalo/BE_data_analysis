e8ee08266d4b97af8e48c44172cd2568c142701f
dacher <mael.dacher@student.isae-supaero.fr>
2026-02-09 13:32:51 +0100

    bootstrap

---
+++ src/bootsrap.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+import sys
+
+from src.utils import logger
+
+
+def setup(repo_name="BE_data_analysis"):
+    if not os.path.exists(repo_name):
+        subprocess.run(
+            ["git", "clone", f"https://github.com/PetitMalo/{repo_name}.git"], check=True
+        )
+
+    os.chdir(repo_name)
+
+    # Installation UV + dépendances
+    subprocess.run("curl -LsSf https://astral.sh/uv/install.sh | sh", shell=True, check=True)
+    os.environ["PATH"] += ":" + os.path.expanduser("~/.cargo/bin")
+    subprocess.run(["uv", "pip", "install", "--system", "-r", "pyproject.toml"], check=True)
+
+    if os.getcwd() not in sys.path:
+        sys.path.append(os.getcwd())
+
+    logger.info(f"Project {repo_name} is ready.")
+
+
+if __name__ == "__main__":
+    setup()
